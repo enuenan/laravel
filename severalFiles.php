4b31a1086ea1e7864521e4d71b476ed7223338d4
enu enan <emoammerfershid@gmail.com>
2021-01-27 00:40:06 +0600

    updated 27-01-2021

---
+++ severalFiles.php
@@ -0,0 +1,5 @@
+<!-- LIMIT THE sentence -->
+{{ Str::limit($service->details, 100) }}
+
+<!-- TO GO BACK TO PREVIOUS PAGE -->
+<a href="javascript:history.back()"><button type="button" class="btn btn-primary">Go Back</button></a>
